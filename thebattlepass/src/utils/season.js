fa919eb1a08975970d348280a727cb0805f2ff74
20rohrichtt <taylorrohrich@gmail.com>
2019-05-21 17:31:43 -0500

    fixed challenge order display

--- thebattlepass/src/utils/season.js
+++ thebattlepass/src/utils/season.js
@@ -1,20 +1,20 @@
-import { orderBy, reverse } from "lodash";
+import { orderBy } from "lodash";
 
 const types = ["metaChallenges", "freeChallenges", "bpChallenges"];
 const flattenChallenges = (challenges, keyOne) => {
   return challenges.reduce(
-    (acc, challenge) => {
+    (acc, challenge, index1) => {
       const { stages, type } = challenge,
         numStages = stages.length,
         key = types[type];
-      const challengeSet = reverse(stages).map((stage, index) => {
-        const keyTwo = `${key}-${index}`;
+      const challengeSet = stages.map((stage, index2) => {
+        const keyTwo = `${key}-${index1}-${index2}`;
         const challengeItem = { ...stage, keyOne, keyTwo };
         return numStages > 1
           ? {
               ...stage,
               total: numStages + 1,
-              stage: index + 1,
+              stage: index2 + 1,
               keyOne,
               keyTwo
             }
@@ -39,22 +39,18 @@ const getSelectedEvents = (events, seasonNumber) => {
       newChallenges = flattenChallenges(challenges, key);
     return { style, title, seasonNumber, challenges: newChallenges };
   });
-  const selected = flattenedEvents.reduce((acc1, event, index1) => {
-    const keyOne = `${seasonNumber}-${index1}`,
+  const selected = flattenedEvents.reduce((acc1, event, index) => {
+    const keyOne = `${seasonNumber}-${index}`,
       challenges = event.challenges,
       eventSet = Object.keys(challenges).reduce((acc2, key) => {
-        const challengeSet = challenges[key].reduce(
-          (acc3, challenge, index2) => {
-            const keyTwo = `${key}-${index2}`,
-              { coordinates, iconId, title } = challenge;
+        const challengeSet = challenges[key].reduce((acc3, challenge) => {
+          const { coordinates, iconId, title, keyTwo } = challenge;
 
-            return {
-              ...acc3,
-              [keyTwo]: { iconId, coordinates, selected: true, title }
-            };
-          },
-          {}
-        );
+          return {
+            ...acc3,
+            [keyTwo]: { iconId, coordinates, selected: true, title }
+          };
+        }, {});
         return { ...acc2, ...challengeSet, selected: true };
       }, {});
     return { ...acc1, [keyOne]: eventSet };
