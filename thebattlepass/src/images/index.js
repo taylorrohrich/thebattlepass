a08e3da6a0e2228ddad3788df37e3e210ba19c5e
20rohrichtt <taylorrohrich@gmail.com>
2019-08-09 16:56:02 -0500

    season 10

--- thebattlepass/src/images/index.js
+++ thebattlepass/src/images/index.js
@@ -3,7 +3,7 @@ import xp from "./xp.png";
 import battlebadge from "./battlebadge.png";
 import battlebadgeGrey from "./battlebadge_grey.png";
 import fnmap9 from "./fnmap9.jpg";
-import fnmap10 from "./fnmap9.jpg";
+import fnmap10 from "./fnmap10.jpg";
 import placeholder from "./placeholder.png";
 import github from "./github.png";
 import mystery from "./mystery.png";
